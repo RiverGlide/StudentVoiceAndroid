2f617d307a526e6cc3f850c988c4856afa6518da
Antony Marcano <talktous+github@riverglide.com>
2010-05-20 19:24:02 +0100

    [am] We have an initial activity with hard coded example universities

--- StudentVoice/src/uk/co/studentvoiceonline/StudentVoice.java
+++ StudentVoice/src/uk/co/studentvoiceonline/StudentVoice.java
@@ -1,13 +1,33 @@
 package uk.co.studentvoiceonline;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class StudentVoice extends Activity {
+public class StudentVoice extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+		setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getUniversities()));
+		getListView().setTextFilterEnabled(true);
     }
+
+	private String[] getUniversities() {
+		return new String[]{
+				"Greenwich University",
+				"North London University",
+				"Oxford University",
+				"Cambridge University",
+				"South Bank University",
+				"Brunel University",
+				"University of Hertfordshire",
+				"Some other University",
+				"Lancaster University",
+				"Leicester University",
+				"This other University"
+				};
+	}
 }
