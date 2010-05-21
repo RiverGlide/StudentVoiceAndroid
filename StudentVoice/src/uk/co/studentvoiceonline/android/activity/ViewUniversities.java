00b94cd944dd1aeaa0694a2cf5686a8ff2a213a2
Antony Marcano <talktous+github@riverglide.com>
2010-05-21 06:38:59 +0100

    [am] Making it all a little more readable

--- StudentVoice/src/uk/co/studentvoiceonline/android/activity/ViewUniversities.java
+++ StudentVoice/src/uk/co/studentvoiceonline/android/activity/ViewUniversities.java
@@ -8,6 +8,7 @@ import java.util.List;
 import uk.co.studentvoiceonline.University;
 import uk.co.studentvoiceonline.UniversityExpert;
 import uk.co.studentvoiceonline.android.R;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,39 +18,60 @@ import android.widget.ListView;
 public class ViewUniversities extends ListActivity {
     private static final int VIEW_DETAILS = 1;
 	private static final int VIEW_UNIVERSITIES_LAYOUT = R.layout.view_universities;
-	public static University currentUniversity; //FIXME
+	public static University theChosenUniversity; //FIXME
 	
 	private List<University> theUniversities;
+	private ViewUniversities thisVeryActivity = this;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         theUniversities = theUniversities();
-        display(theUniversities);
-        enableUserToFilterTextByTyping();
+        showAListOf(theUniversities);
+        theUserCanFilterThemByTyping();
     }
     
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent i = new Intent(this, ViewUniversityDetails.class);
-        University[] universities = new University[]{};
-        universities = theUniversities.toArray(universities);
-        currentUniversity = universities[position];
-        startActivityForResult(i, VIEW_DETAILS);
+    protected void onListItemClick(ListView l, View v, int inThisPosition, long id) {
+        super.onListItemClick(l, v, inThisPosition, id);
+
+        theChosenUniversity = theUniversity(inThisPosition);
+        showItsDetails();
     }
 
 	////////////////////////////////////////////////////////
-	private void display(List<University> theUniversities) {
+	private void showAListOf(List<University> theUniversities) {
 		setContentView(VIEW_UNIVERSITIES_LAYOUT);
-		setListAdapter(forListing(theUniversities, during(this)));
+		setListAdapter(forListing(theUniversities, during(thisVeryActivity)));
 	}
 
-	private void enableUserToFilterTextByTyping() {
+	private void theUserCanFilterThemByTyping() {
 		getListView().setTextFilterEnabled(true);
 	}
 
 	private List<University> theUniversities() {
 		return new UniversityExpert().whatAreTheUniversities();
 	}
+	
+	private University theUniversity(int fromThisPosition) {
+		University[] universities = new University[]{};
+        universities = theUniversities.toArray(universities);
+        return universities[fromThisPosition];
+	}
+
+	private void showItsDetails() {
+		transitionTo(VIEW_DETAILS, definedByThe(ViewUniversityDetails.class));
+	}
+
+	private void transitionTo(int requestCode, Class<? extends Activity> activityClass) {
+		Intent i = new Intent(this, activityClass);
+        startActivityForResult(i, requestCode);
+	}
+
+	private Class<? extends Activity> definedByThe(
+			Class<? extends Activity> activityClass) {
+		return activityClass;
+	}
+
 }
