41042cb6f7c46fa8e71787f9d96111312a753e4d
Antony Marcano <talktous+github@riverglide.com>
2010-05-25 13:07:07 +0100

    [cm,am,mt] updated list of Uni's to London Uni's

--- StudentVoice/src/uk/co/studentvoiceonline/UniversityExpert.java
+++ StudentVoice/src/uk/co/studentvoiceonline/UniversityExpert.java
@@ -8,17 +8,27 @@ public class UniversityExpert {
 
 	public List<University> whatAreTheUniversities() {
 		String[] uniNames =  new String[]{
-				"Greenwich University",
-				"North London University",
-				"Oxford University",
-				"Cambridge University",
-				"South Bank University",
-				"Brunel University",
-				"University of Hertfordshire",
-				"Some other University",
-				"Lancaster University",
-				"Leicester University",
-				"This other University"
+				"University College London",
+				"King's College London", 
+				"Birkbeck, University of London", 
+				"American Intercontinental University", 
+				"St George's, University of London",
+				"London South Bank University",
+				"University of East London", 
+				"University of London Institute in Paris", 
+				"London Metropolitan University",
+				"Royal Veterinary College", 
+				"University of The Arts London",
+				"London School of Economics and Political Science", 
+				"Central School of Speech and Drama",
+				"Royal Holloway",
+				"Heythrop College", 
+				"The School of Pharmacy", 
+				"Queen Mary, University of London",
+				"Goldsmiths, University of London",
+				"School of Oriental and African Studies", 
+				"Court auld Institute of Art",
+				"University of Greenwich"
 				};
 		
 		ArrayList<University> universityList = new ArrayList<University>();
