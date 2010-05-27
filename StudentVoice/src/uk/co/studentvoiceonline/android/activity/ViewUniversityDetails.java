d34c54fdf4454274b1c0ee5ffaef07f0806b72c6
Antony Marcano <talktous+github@riverglide.com>
2010-05-27 12:57:12 +0100

    [am,mt,cm] ViewUniversityDetails code is easier to read

--- StudentVoice/src/uk/co/studentvoiceonline/android/activity/ViewUniversityDetails.java
+++ StudentVoice/src/uk/co/studentvoiceonline/android/activity/ViewUniversityDetails.java
@@ -1,28 +1,43 @@
 package uk.co.studentvoiceonline.android.activity;
 
+import uk.co.studentvoiceonline.University;
 import uk.co.studentvoiceonline.android.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class ViewUniversityDetails extends Activity {
-
+		
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.view_university_detail);
-		TextView text = (TextView)findViewById(R.id.university_name);
-		text.setText(ViewUniversities.theChosenUniversity.name() + "\n");
-		
+
+		University theChosenUniversity = ViewUniversities.theChosenUniversity; 
+		showTheNameOf(theChosenUniversity);
+		showALinkToTheUcasWebsiteFor(theChosenUniversity);
+	}
+
+	private void showALinkToTheUcasWebsiteFor(University theChosenUniversity) {
 		TextView ucasLink = (TextView)findViewById(R.id.ucas_link);
-		
-		ucasLink.setText(Html.fromHtml(
+		ucasLink.setText(ucasLinkFor(theChosenUniversity));
+		ucasLink.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	private Spanned ucasLinkFor(University theChosenUniversity) {
+		return Html.fromHtml(
 				"<a href=\"" 
-				+ ViewUniversities.theChosenUniversity.ucasLink()
+				+ theChosenUniversity.ucasLink()
 				+"\">"
 				+ "View on UCAS website"
-				+"</a>"));
-		ucasLink.setMovementMethod(LinkMovementMethod.getInstance());
+				+"</a>"
+				);
+	}
+
+	private void showTheNameOf(University theChosenUniversity) {
+		TextView text = (TextView)findViewById(R.id.university_name);
+		text.setText(theChosenUniversity.name() + "\n");
 	}
 }
